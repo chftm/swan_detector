f92be8af1517dc79e3b309386a22a65ee68260ab
flexagoon <code@fxgn.dev>
2023-05-21 02:48:04 +0300

    ai: add __name__ check

--- AI/AI2.py
+++ AI/AI2.py
@@ -4,21 +4,8 @@ from matplotlib import pyplot as plt
 from keras import Sequential
 from ultralytics import YOLO
 import cv2, os
+
 model_SEG = YOLO("model_YOLO.pt")
-
-
-def YOLO_predict(src):
-    output_size = (800, 400)
-    image = cv2.resize(cv2.imread(src), output_size)
-
-    results = model_SEG.predict(image)
-    predicted_boxes = results[0].boxes.xyxy
-
-    image_bgr = cv2.imread(src, cv2.IMREAD_COLOR)
-    results = []
-    for box in predicted_boxes:
-        results.append(image[int(box[1]):int(box[3]), int(box[0]):int(box[2])])
-    return results
     
 model = tf.keras.models.Sequential([
     tf.keras.layers.InputLayer(input_shape=(400, 800, 3)),
@@ -32,25 +19,20 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dense(3, activation="softmax"),
 ])
-print(model.summary())
-model.compile(optimizer="adam", loss=tf.keras.losses.categorical_crossentropy)
-dirs = ["klikun", "maliy", "shipun"]
 
-c = 0
-for i in range(1, 3001):
-    os.system("cls")
-    print(f"{c}/3000 :)")
-    c += 1
-    for dir_index in range(3):
-        images = YOLO_predict(f"{dirs[dir_index]}/images/{i}.jpg")
-        for img in images:
-            image = cv2.resize(img, (800, 400))
-            y_data = [0, 0, 0]
-            y_data[dir_index] = 1
-            loss = model.train_on_batch(np.expand_dims(image, axis=0), np.array([y_data]))
-            print(loss)
+def YOLO_predict(src):
+    output_size = (800, 400)
+    image = cv2.resize(cv2.imread(src), output_size)
 
-model.save("model.h5")
+    results = model_SEG.predict(image)
+    predicted_boxes = results[0].boxes.xyxy
+
+    image_bgr = cv2.imread(src, cv2.IMREAD_COLOR)
+    results = []
+    for box in predicted_boxes:
+        results.append(image[int(box[1]):int(box[3]), int(box[0]):int(box[2])])
+    return results
+
 def PREDICT(path:str) -> dict:
     images = YOLO_predict(path)
     FINAL_model = tf.keras.models.load_model("model.h5")
@@ -60,4 +42,25 @@ def PREDICT(path:str) -> dict:
         res[names[np.argmax(FINAL_model.predict(np.expand_dims(cv2.resize(image, (800, 400)), axis=0)))]] += 1
     return res 
 
-print(PREDICT("klikun/images/2969.jpg"))
+if __name__ == '__main__':
+    print(model.summary())
+    model.compile(optimizer="adam", loss=tf.keras.losses.categorical_crossentropy)
+    dirs = ["klikun", "maliy", "shipun"]
+
+    c = 0
+    for i in range(1, 3001):
+        os.system("cls")
+        print(f"{c}/3000 :)")
+        c += 1
+        for dir_index in range(3):
+            images = YOLO_predict(f"{dirs[dir_index]}/images/{i}.jpg")
+            for img in images:
+                image = cv2.resize(img, (800, 400))
+                y_data = [0, 0, 0]
+                y_data[dir_index] = 1
+                loss = model.train_on_batch(np.expand_dims(image, axis=0), np.array([y_data]))
+                print(loss)
+
+    model.save("model.h5")
+
+    print(PREDICT("klikun/images/2969.jpg"))
