fc9de3e3ee01aeefe20043241cb99eba01a77da1
Ultrageopro1966 <120571667+Ultrageopro1966@users.noreply.github.com>
2023-05-21 10:26:40 +0300

    Update AI2.py

--- AI/AI2.py
+++ AI/AI2.py
@@ -4,21 +4,8 @@ from matplotlib import pyplot as plt
 from keras import Sequential
 from ultralytics import YOLO
 import cv2, os
-
 model_SEG = YOLO("model_YOLO.pt")
-    
-model = tf.keras.models.Sequential([
-    tf.keras.layers.InputLayer(input_shape=(400, 800, 3)),
-    tf.keras.layers.Conv2D(96, padding="same", activation="relu", kernel_size=(2, 2)),
-    tf.keras.layers.MaxPooling2D(), 
-    tf.keras.layers.Conv2D(64, padding="same", activation="relu", kernel_size=(2, 2)),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, padding="same", activation="relu", kernel_size=(2, 2)),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation="relu"),
-    tf.keras.layers.Dense(3, activation="softmax"),
-])
+
 
 def YOLO_predict(src):
     output_size = (800, 400)
@@ -32,8 +19,44 @@ def YOLO_predict(src):
     for box in predicted_boxes:
         results.append(image[int(box[1]):int(box[3]), int(box[0]):int(box[2])])
     return results
+    
+model = tf.keras.models.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(400, 800, 3)),
+    tf.keras.layers.Conv2D(64, padding="same", activation="relu", kernel_size=(2, 2)),
+    tf.keras.layers.MaxPooling2D(), 
+    tf.keras.layers.Conv2D(48, padding="same", activation="relu", kernel_size=(2, 2)),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, padding="same", activation="relu", kernel_size=(2, 2)),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(3, activation="softmax"),
+])
+print(model.summary())
+model.compile(optimizer="adam", loss=tf.keras.losses.categorical_crossentropy)
+dirs = ["klikun", "maliy", "shipun"]
 
-def PREDICT(path:str) -> dict:
+c = 0
+for i in range(1, 3001):
+    os.system("cls")
+    print(f"{c}/3000 :)")
+    c += 1
+    for dir_index in range(3):
+        images = YOLO_predict(f"{dirs[dir_index]}/images/{i}.jpg")
+        for img in images:
+            image = cv2.resize(img, (800, 400))
+            x_data, y_data = [], []
+            res = [0, 0, 0]
+            res[dir_index] = 1
+            x_data.append(image)
+            y_data.append(np.array(res))
+            print(y_data)
+        model.fit(np.array(x_data), np.array(y_data))
+
+
+
+model.save("model.h5")
+def RESULT(path:str) -> dict:
     images = YOLO_predict(path)
     FINAL_model = tf.keras.models.load_model("model.h5")
     res = {"кликун":0, "малый":0, "щипун":0}
@@ -42,25 +65,4 @@ def PREDICT(path:str) -> dict:
         res[names[np.argmax(FINAL_model.predict(np.expand_dims(cv2.resize(image, (800, 400)), axis=0)))]] += 1
     return res 
 
-if __name__ == '__main__':
-    print(model.summary())
-    model.compile(optimizer="adam", loss=tf.keras.losses.categorical_crossentropy)
-    dirs = ["klikun", "maliy", "shipun"]
-
-    c = 0
-    for i in range(1, 3001):
-        os.system("cls")
-        print(f"{c}/3000 :)")
-        c += 1
-        for dir_index in range(3):
-            images = YOLO_predict(f"{dirs[dir_index]}/images/{i}.jpg")
-            for img in images:
-                image = cv2.resize(img, (800, 400))
-                y_data = [0, 0, 0]
-                y_data[dir_index] = 1
-                loss = model.train_on_batch(np.expand_dims(image, axis=0), np.array([y_data]))
-                print(loss)
-
-    model.save("model.h5")
-
-    print(PREDICT("klikun/images/2969.jpg"))
+print(RESULT("klikun/images/2969.jpg"))
