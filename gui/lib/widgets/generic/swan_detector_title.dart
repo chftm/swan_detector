58ab2246b6f3a9d5169441ba2605ee5cb8552ebb
flexagoon <code@fxgn.dev>
2023-05-21 03:37:57 +0300

    gui: add a "made by" text to title

--- gui/lib/widgets/generic/swan_detector_title.dart
+++ gui/lib/widgets/generic/swan_detector_title.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SwanDetectorTitle extends StatelessWidget {
   const SwanDetectorTitle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    final dimmedColor = Theme.of(context).colorScheme.primary.withOpacity(0.3);
+
+    return SizedBox(
       width: double.infinity,
-      child: Text(
-        'Swan detector',
-        style: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.w700,
-        ),
-        textAlign: TextAlign.center,
-        overflow: TextOverflow.ellipsis,
-        maxLines: 2,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Text(
+            'Swan detector',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+            ),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'made by',
+                style: TextStyle(
+                  color: dimmedColor,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(width: 10),
+              SvgPicture.asset(
+                'assets/icons/logo.svg',
+                height: 25,
+                colorFilter: ColorFilter.mode(
+                  dimmedColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
